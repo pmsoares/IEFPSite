1d09eb51d449c91ec2c16a647d99645ce6f50b61
Pedro Soares <pmsoares@gmail.com>
2023-02-23 17:54:21 +0000

    Sessão de 23/02/2023

--- IEFPSiteWeb/Controllers/CategoryController.cs
+++ IEFPSiteWeb/Controllers/CategoryController.cs
@@ -18,5 +18,12 @@ namespace IEFPSiteWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
